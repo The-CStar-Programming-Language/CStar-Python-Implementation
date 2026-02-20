9ab8ce47520af4de92f55f7d7d33b7e1c460eead
nobody_on_earth <reddeadarthurfan1889@gmail.com>
2026-02-20 12:25:26 -0600

    Update tokenizer.py

--- interpreter/tokenizer.py
+++ interpreter/tokenizer.py
@@ -1,1 +1,26 @@
 
+import re, sys
+def tokenize(line):
+	content = re.search(r"\(([^)]+)\)", line)
+	if content:
+		content = content.group(1).split('|')
+		command = content[0]
+		command = command.strip()
+		if len(content) == 2:
+			value = content[1]
+			return [command, value]
+		elif len(content) == 3:
+			var = content[2]
+			value = content[1]
+			return [command, value, var]
+		elif len(command) == 4:
+			# assumes command is "init"
+			try:
+				type = content[1]
+				name = content[2]
+				value = content[3]
+				return [command, type, name, value]
+			except:
+				pass
+	else:
+		print("No command or value was found.")
